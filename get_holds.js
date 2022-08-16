b5e6e43b02955901b6b0179974f802dd7f65f20d
DennisDv24 <jokarboxing@gmail.com>
2022-08-16 20:26:57 +0200

    getting holders, need to work with the pagination

--- get_holds.js
+++ get_holds.js
@@ -1,15 +1,17 @@
 const ethers = require('ethers')
 const axios = require('axios')
-require('dotenv').config({ path: './.env' });
+require('dotenv').config({ path: './.env' })
 
 const addr = '0x0E124A7c9b378d7276590b6b8b676a75ff923a03'
-const API = process.env.ETHERSCAN_API
+const milady_addr = '0x5af0d9827e0c53e4799bb226655a1de152a425a5'
+const ETHERSCAN_API = process.env.ETHERSCAN_API
+const COVALENT_API = process.env.COVALENT_API
 
 const getEtherscanProvider = () => 
-	new ethers.providers.EtherscanProvider('mainnet', API);
+	new ethers.providers.EtherscanProvider('mainnet', API)
 
-const get721Txs = async (addr) => {
-	const currentBlock = await getEtherscanProvider().getBlockNumber();
+const get721Txs = async addr => {
+	const currentBlock = await getEtherscanProvider().getBlockNumber()
 	const txs = await axios.get(`\
 		https://api.etherscan.io/api\
 		?module=account\
@@ -17,7 +19,7 @@ const get721Txs = async (addr) => {
 		&address=${addr}\
 		&startblock=0\
 		&endblock=${currentBlock}\
-		&apikey=${API}\
+		&apikey=${ETHERSCAN_API}\
 	`.replace(/\t/g, ''))
 	return txs.data.result
 }
@@ -26,23 +28,26 @@ const get721HoldsFromTxs = addr => async txs => {
 	addr = addr.toLowerCase()
 	const tokensHeld = {}
 	txs.forEach(tx => {
-		const colle = tx.contractAddress;
-		if(!(colle in tokensHeld))
-			tokensHeld[colle] = 0
-		if(tx.to === addr)
-			tokensHeld[colle]++
-		else
-			tokensHeld[colle]--
+		const colle = tx.contractAddress
+		if(!(colle in tokensHeld)) tokensHeld[colle] = 0
+		if(tx.to === addr) tokensHeld[colle]++
+		else tokensHeld[colle]--
 	})
 	return tokensHeld
 }
+
+const getHoldersOf721 = async contractAddr =>
+	(await axios.get(`\
+		https://api.covalenthq.com/v1/1/tokens/\
+		${contractAddr}\
+		/token_holders/?key=${COVALENT_API}\
+	`.replace(/\t/g, ''))).data.data
 
 const get721Holds = async (addr) => 
 	get721Txs(addr).then(get721HoldsFromTxs(addr))
 
 
 get721Holds(addr).then(console.log)
-
-//get721HoldsFromTxs(addr, await get721Txs(addr)).then(console.log);
+getHoldersOf721(milady_addr).then(console.log)
 
 
