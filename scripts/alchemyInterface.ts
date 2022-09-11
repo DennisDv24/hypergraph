e6bb5e4d9f4d903e1b3463a17523f3368903be10
DennisDv24 <jokarboxing@gmail.com>
2022-09-11 20:21:56 +0200

    ts integration

--- scripts/alchemyInterface.ts
+++ scripts/alchemyInterface.ts
@@ -1,21 +1,24 @@
-import {_getCollectionHolders} from './alchemyUtilities'
+import {
+	_getCollectionHolders,
+	HoldersAndBals,
+	OnlyHolders
+} from './alchemyUtilities'
 
-const getCollectionHolders = async (contractAddr: string): Promise<string[]> =>
-	await _getCollectionHolders(contractAddr, false)
+const getCollectionHolders = async (contractAddr: string): Promise<OnlyHolders> =>
+	await _getCollectionHolders(contractAddr) as OnlyHolders
 
-type Holdings = {[addr: string]: number}
-
-const getHoldersBalancesForCollection = async (cAddr: string): Promise<Holdings> => {
-	const holds = await _getCollectionHolders(cAddr, true)
-	return holds.reduce((a:any, v:any) => (
+type FormatedHolds = {[addr: string]: number}
+const getHoldersBalancesForCollection = async (cAddr: string): Promise<FormatedHolds> => {
+	const holds = (await _getCollectionHolders(cAddr, true)) as HoldersAndBals
+	return holds.reduce((a, v) => (
 		{...a, [v.ownerAddress]: v.tokenBalances.length}
-	))
+	), {})
 }
 
 const main = async () => {
-	const holds = await getCollectionHolders(
-		'0x1352149Cd78D686043B504e7e7D96C5946b0C39c'
-	)
+	const addr = '0x1352149Cd78D686043B504e7e7D96C5946b0C39c'
+	const bals = await getHoldersBalancesForCollection(addr)
+	const holds = await getCollectionHolders(addr)
 	console.log(holds)
 }
 
--- scripts/alchemyUtilities.ts
+++ scripts/alchemyUtilities.ts
@@ -5,17 +5,16 @@ const ALCHEMY_API = process.env.ALCHEMY_API
 const alchemySdk = require('api')('@alchemy-docs/v1.0#u2rm9ol7vhuzf7')
 alchemySdk.server('https://eth-mainnet.g.alchemy.com/nft/v2');
 
-type OnlyHolders = string[]
-type HoldersAndBals = [{
+export type Holders = string[]
+export type HoldersAndBals = [{
 	ownerAddress: string, tokenBalances: [{
 		tokenId: string, balance: number
 	}]
 }]
-type Holds = OnlyHolders | HoldersAndBals
 
 export const _getCollectionHolders = async (
 	cAddr: string, showBals?: boolean
-): Promise<Holds> => (
+): Promise<Holders | HoldersAndBals> => (
 	(await alchemySdk.getOwnersForCollection({
 		contractAddress: cAddr,
 		withTokenBalances: showBals,
@@ -29,7 +28,6 @@ const main = async () => {
 		'0x1352149Cd78D686043B504e7e7D96C5946b0C39c', true
 	)
 	console.log(holds[0])
-
 }
 
 main()
